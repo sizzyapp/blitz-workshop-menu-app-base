87745e2e43651bb93bc49dc3a5dbdb1db4e7a202
Fran Zekan <zekan.fran369@gmail.com>
2022-09-28 20:11:05 +0200

    3-forms/4-connect-mutation-to-form

--- pages/menus/new.tsx
+++ pages/menus/new.tsx
@@ -1,8 +1,25 @@
+import { useMutation } from "@blitzjs/rpc"
+
+import createMenuFn from "app/menu/mutations/createMenu"
+import { FORM_ERROR } from "app/core/components/Form"
 import { MenuForm } from "app/menu/components/MenuForm"
 
 const NewMenuPage = () => {
+  const [createMenu] = useMutation(createMenuFn)
+
   const onSubmit = async (data) => {
-    console.log(data)
+    try {
+      const res = await createMenu(data)
+      if (!res.ok) {
+        return {
+          [res.errorField]: res.errorMessage
+        }
+      }
+
+      console.log("Created")
+    } catch (err) {
+      return { [FORM_ERROR]: err.toString() }
+    }
   }
 
   return (
