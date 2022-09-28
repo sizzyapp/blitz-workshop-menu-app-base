0804cc742e2ef2bdb4a60ca7b6878f7a74b342dd
kitze <kristijan.mkd@gmail.com>
2022-09-28 17:42:13 +0200

    extract RootErrorFallback, change functions to const (like the rest of the app)

---
+++ components/RootErrorFallback.tsx
@@ -0,0 +1,23 @@
+import { ErrorComponent, ErrorFallbackProps } from "@blitzjs/next"
+import { AuthenticationError, AuthorizationError } from "blitz"
+import React from "react"
+
+export const RootErrorFallback: React.FC<ErrorFallbackProps> = ({ error }) => {
+  if (error instanceof AuthenticationError) {
+    return <div>Error: You are not authenticated</div>
+  } else if (error instanceof AuthorizationError) {
+    return (
+      <ErrorComponent
+        statusCode={error.statusCode}
+        title="Sorry, you are not authorized to access this"
+      />
+    )
+  } else {
+    return (
+      <ErrorComponent
+        statusCode={(error as any)?.statusCode || 400}
+        title={error.message || error.name}
+      />
+    )
+  }
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,38 +1,17 @@
-import { ErrorFallbackProps, ErrorComponent, ErrorBoundary, AppProps } from "@blitzjs/next"
-import { AuthenticationError, AuthorizationError } from "blitz"
-import React, { Suspense } from "react"
-import { withBlitz } from "app/blitz-client"
+import { AppProps, ErrorBoundary } from "@blitzjs/next"
 import { AppShell, Loader, MantineProvider } from "@mantine/core"
+import { withBlitz } from "app/blitz-client"
+import { RootErrorFallback } from "components/RootErrorFallback"
+import React, { Suspense } from "react"
 
-function RootErrorFallback({ error }: ErrorFallbackProps) {
-  if (error instanceof AuthenticationError) {
-    return <div>Error: You are not authenticated</div>
-  } else if (error instanceof AuthorizationError) {
-    return (
-      <ErrorComponent
-        statusCode={error.statusCode}
-        title="Sorry, you are not authorized to access this"
-      />
-    )
-  } else {
-    return (
-      <ErrorComponent
-        statusCode={(error as any)?.statusCode || 400}
-        title={error.message || error.name}
-      />
-    )
-  }
-}
-
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <ErrorBoundary FallbackComponent={RootErrorFallback}>
       <MantineProvider
         withGlobalStyles
         withNormalizeCSS
         theme={{
-          /** Put your mantine theme override here */
-          colorScheme: "light",
+          colorScheme: "light"
         }}
       >
         <AppShell>
