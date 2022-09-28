54847616f80550136fefb5bb0efba33e3582344a
Fran Zekan <zekan.fran369@gmail.com>
2022-09-28 20:07:13 +0200

    2-queries/7-public-menu-query

--- pages/m/[slug].tsx
+++ pages/m/[slug].tsx
@@ -1,20 +1,23 @@
 import React from "react"
 import { useParam } from "@blitzjs/next"
+import { useQuery } from "@blitzjs/rpc"
 import Head from "next/head"
 
 import getPublicMenu from "app/menu/queries/getPublicMenu"
 
 const PublicMenuPage: React.FC = () => {
   const slug = useParam("slug", "string")!
+  const [menu] = useQuery(getPublicMenu, { slug })
 
   return (
     <>
       <Head>
-        <title>{slug}</title>
+        <title>{menu.title}</title>
       </Head>
 
       <div>
-        <h1>{slug}</h1>
+        <h1>{menu.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: menu.body }} />
       </div>
     </>
   )
